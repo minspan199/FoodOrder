32810995f7b789cd670ade487ab58a33a72ae48e
minspan1999 <minspan@users.noreply.github.com>
2020-01-06 18:30:22 -0500

    """

--- app/src/main/java/com/michael/pan/foodorder/MainActivity.java
+++ app/src/main/java/com/michael/pan/foodorder/MainActivity.java
@@ -2,13 +2,37 @@ package com.michael.pan.foodorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+	Button btnSignIn, btnSignUp;
+	TextView txtSlogan;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		btnSignIn = findViewById(R.id.btnSignIn);
+		btnSignUp = findViewById(R.id.btnSignUp);
+		txtSlogan = findViewById(R.id.txtSlogan);
+		Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/Exo2-Regular.ttf");
+		txtSlogan.setTypeface(typeface);
+		btnSignIn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+		btnSignUp.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
 	}
 }
